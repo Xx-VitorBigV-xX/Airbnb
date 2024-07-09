314e073aff33ecc871f76f77cd112dfe95cb2863
Xx-VitorBigV-xX <129459463+Xx-VitorBigV-xX@users.noreply.github.com>
2024-07-09 10:48:19 -0300

    inicialização

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+
+import "./App.css";
+import {Route, Routes} from "react-router-dom";
+import IndexPage from "./pages/IndexPage";
+
+function App() {
+  return (
+  <Routes>
+    <Route index element={<IndexPage />} />
+  </Routes>
+   
+  )
+}
+
+export default App
