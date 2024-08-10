e50efbd31f5ffb6fce8f27b5a50dd35aad1391b6
Xx-VitorBigV-xX <129459463+Xx-VitorBigV-xX@users.noreply.github.com>
2024-08-10 18:51:40 -0300

    connecting Mongo Atlas + connecting register user and login user with mongodb Atlas

--- api/index.js
+++ api/index.js
@@ -2,21 +2,45 @@
 
 const express = require('express');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
+require('dotenv').config()
+const mongoose = require('mongoose');
+const User = require('./models/User.js');
 const app = express();
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 app.use(express.json());
 app.use(cors({
 credentials: true,
 origin: 'http://localhost:5173',
 })); 
-
+mongoose.connect(process.env.MONGO_URL);
 
 app.get('/test', (req,res)=>{
     res.json('test ok');
 });
-app.post('/register',(req,res) => {
+app.post('/register', async (req,res) => {
     const {name,email,password} = req.body; 
-    res.json({name,email,password});
+    try {
+        const userDoc =  await User.create({
+            name,
+            email,
+            password:bcrypt.hashSync(password,bcryptSalt),
+        });
+        res.json(userDoc);
+    } catch (e){
+        res.status(422).json(e);
+    } 
 });
+
+app.post('/login', async (req,res)=>{
+    const{email,password}  = req.body;
+    const userDoc = await User.findOne({email});
+    if(userDoc){
+        res.json('found');
+    }else {
+        res.json('not found');
+    }
+})
 
 app.listen(4000);
